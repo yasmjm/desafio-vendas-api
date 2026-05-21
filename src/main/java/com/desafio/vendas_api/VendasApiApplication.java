96eae72c6b69dc15df0da7dbf7caf1c54a0d8b7c
mariayasmimsousaalmeida@gmail.com <yasmjm>
2026-05-21 20:58:13 -0300

    feat: ativar suporte a auditoria JPA na classe principal da aplicação

--- src/main/java/com/desafio/vendas_api/VendasApiApplication.java
+++ src/main/java/com/desafio/vendas_api/VendasApiApplication.java
@@ -2,12 +2,14 @@ package com.desafio.vendas_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+
 public class VendasApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VendasApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApiApplication.class, args);
+    }
 }
